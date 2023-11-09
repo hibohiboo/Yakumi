19a68ec898f932996810f5ff1fe83ebd17bda471
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-09 20:43:43 +0900

    add blueprint #1

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,16 @@
+import { Button } from '@blueprintjs/core';
 import { selectDirectory } from './domain/fileSystem/fileReader';
-
 function App() {
   return (
     <div>
-      <button
+      <Button
+        icon="refresh"
         onClick={() => {
           selectDirectory();
         }}
       >
-        ファイルを読み込む。
-      </button>
+        ファイル読み込み
+      </Button>
     </div>
   );
 }
