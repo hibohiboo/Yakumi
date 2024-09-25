c83231e409862ab0caa25a80554cf18acf2cff92
hibohiboo <hibohiboo66+github@gmail.com>
2024-09-25 23:40:46 +0900

    add deploy functions AB#464

--- packages/batch-functions/src/functions/eventGridBlobTrigger1.ts
+++ packages/batch-functions/src/functions/eventGridBlobTrigger1.ts
@@ -1,12 +1,17 @@
-import { app, InvocationContext } from "@azure/functions";
+import { app, InvocationContext } from '@azure/functions';
 
-export async function eventGridBlobTrigger1(blob: Buffer, context: InvocationContext): Promise<void> {
-    context.log(`Storage blob function processed blob "${context.triggerMetadata.name}" with size ${blob.length} bytes`);
+export async function eventGridBlobTrigger1(
+  blob: unknown, // Buffer,
+  context: InvocationContext,
+): Promise<void> {
+  context.log(
+    `Storage blob function processed blob "${context.triggerMetadata?.name}" with size ${(blob as Buffer).length} bytes`,
+  );
 }
 
 app.storageBlob('eventGridBlobTrigger1', {
-    path: 'samples-workitems/batch-functions',
-    source: 'EventGrid',
-    connection: '0d5e76_STORAGE',
-    handler: eventGridBlobTrigger1
+  path: 'samples-workitems/batch-functions',
+  source: 'EventGrid',
+  connection: '0d5e76_STORAGE',
+  handler: eventGridBlobTrigger1,
 });
