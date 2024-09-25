eda2a70ffa74710d019bddb0f1cd95a1a7cbb58e
hibohiboo <hibohiboo66+github@gmail.com>
2024-09-26 08:09:41 +0900

    add logの確認 AB#464

--- packages/batch-functions/src/functions/eventGridBlobTrigger1.ts
+++ packages/batch-functions/src/functions/eventGridBlobTrigger1.ts
@@ -4,13 +4,21 @@ export async function BlobTriggerEventGrid(
   blob: unknown, // Buffer,
   context: InvocationContext,
 ): Promise<void> {
+  console.log('context', JSON.stringify(context));
+  if (!Buffer.isBuffer(blob)) {
+    context.warn('blob is not buffer');
+    return;
+  }
   context.log(
-    `Storage blob function processed blob "${context.triggerMetadata?.name}" with size ${(blob as Buffer).length} bytes`,
+    `Storage blob function processed blob "${context.triggerMetadata?.name}" with size ${blob.length} bytes`,
   );
+  const blobContent = blob.toString('utf-8');
+  const obj = JSON.parse(blobContent);
+  context.log('obj', obj);
 }
 
 app.storageBlob('BlobTriggerEventGrid', {
-  path: '$web',
+  path: 'web',
   source: 'EventGrid',
   connection: 'TARGET_STORAGE_ACCOUNT',
   handler: BlobTriggerEventGrid,
